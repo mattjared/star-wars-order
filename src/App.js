41fa0ceaaaf811ea3a4028afefbea4ab6a83f35b
Matt Jared <mattjared9@gmail.com>
2018-02-02 20:47:40 -0600

    Setup data and cleanup project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 
 class App extends Component {
-  render() {
+  render() {    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {data.map((d, i) => {
+            return (
+              <div className="push">
+                <li>{d.name}</li>
+                <li>{d.bio}</li>
+                <li>{d.date}</li>
+              </div>
+            )
+          })}
       </div>
     );
   }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+		"name": "The Phantom Menace",
+		"bio": "I happen to have a chance cube right here",
+		"date": "1999",
+  },
+	{
+		"name": "A New Hope",
+		"bio": "That's no moon",
+		"date": "1977",
+	},
+	{
+		"name": "The Force Awakens",
+		"bio": "TALAMAPARKWA",
+		"date": "2015",
+	}
+];
+
+export default data;
